8c71a371bfd365b0bae06f9ef83e27e3edd75f2b
Yangxianpeng26 <yangxianoeng26>
2020-11-01 15:30:57 +0800

    feat:项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
+//全局样式
+import './style/index.less'
+
+
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
